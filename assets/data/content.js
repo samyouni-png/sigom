f6e2dcf64d3ca032909785b324e6b21f5f8d7978
samyouni-png <samy.ouni@hotmail.com>
2026-08-06 10:55:17 +0200

    Add files via upload

---
+++ assets/data/content.js
@@ -0,0 +1,17 @@
+window.SIGOM_CONTENT = {
+  projectSchema: {
+    title: "",
+    typology: "",
+    location: "",
+    mission: "",
+    context: "",
+    deliverables: [],
+    status: "",
+    image: "",
+    imageAlt: "",
+    published: false
+  },
+  projects: [],
+  qualifications: [],
+  editorial: []
+};
